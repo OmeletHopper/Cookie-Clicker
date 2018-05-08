0533f2760d56be24651f001fe4da146a08bbc496
OmeletHopper <me@jonnyarcher.com>
2018-05-07 21:19:32 -0500

    Small fixups: Also fixes bug where restoreData wasn't called until after UI loaded

--- Cookie Clicker/AppDelegate.swift
+++ Cookie Clicker/AppDelegate.swift
@@ -16,12 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        restoreData();
         return true;
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        UpgradeViewController().saveEverything();
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -34,13 +36,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        restoreData();
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        UpgradeViewController().saveEverything();
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        UpgradeViewController().saveEverything();
     }
 
 
