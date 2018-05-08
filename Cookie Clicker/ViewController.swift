7579bb6855ef071325c9f98687a359e234c64ee6
OmeletHopper <me@jonnyarcher.com>
2018-05-07 20:17:58 -0500

    Change Copyright, change to use restoreData, and some variable renames

--- Cookie Clicker/ViewController.swift
+++ Cookie Clicker/ViewController.swift
@@ -3,7 +3,7 @@
 //  Cookie Clicker
 //
 //  Created by Jonathan Archer on 6/23/17.
-//  Copyright © 2017 Jack Archer Enterprises. All rights reserved.
+//  Copyright © 2017 Jonny Archer. All rights reserved.
 //
 
 import UIKit
@@ -12,106 +12,23 @@ import UIKit
 var CookieVariable = 0.0
 
 var CursorVariable = 15.0
-var CursorCount = 0
+var CursorAmount = 0
 
 var GrandmaVariable = 100.0
-var GrandmaCount = 0
+var GrandmaAmount = 0
 
 var MineVariable = 500.0
-var MineCount = 0
+var MineAmount = 0
 
 var FactoryVariable = 2000.0
-var FactoryCount = 0
+var FactoryAmount = 0
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         assignBackground()
-        
-        var fileName = "CookieSave"
-        var DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        
-        var fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("txt")
-        print("FilePath: \(fileURL.path)")
-        
-        do {
-            // Read the file contents
-            CookieVariable = try Double(String(contentsOf: fileURL))!
-        }
-        catch let error as NSError {
-            print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
-        }
-        
-        
                 
-        fileName = "CursorAmountSave"
-        DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        
-        fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("txt")
-        print("FilePath: \(fileURL.path)")
-        
-        do {
-            // Read the file contents
-            CursorCount = try Int(String(contentsOf: fileURL))!
-        } catch let error as NSError {
-            print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
-        }
-        
-        CursorVariable = 15 * pow(1.15, Double(CursorCount))
-        
-        
-        fileName = "GrandmaAmountSave"
-        DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        
-        fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("txt")
-        print("FilePath: \(fileURL.path)")
-        
-        do {
-            // Read the file contents
-            GrandmaCount = try Int(String(contentsOf: fileURL))!
-        } catch let error as NSError {
-            print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
-        }
-        
-        GrandmaVariable = 100 * pow(1.15, Double(GrandmaCount))
-        
-        
-        fileName = "MineAmountSave"
-        DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        
-        fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("txt")
-        print("FilePath: \(fileURL.path)")
-        
-        do {
-            // Read the file contents
-            MineCount = try Int(String(contentsOf: fileURL))!
-        } catch let error as NSError {
-            print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
-        }
-        
-        MineVariable = 500 * pow(1.15, Double(MineCount))
-        
-        
-        fileName = "FactoryAmountSave"
-        DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        
-        fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("txt")
-        print("FilePath: \(fileURL.path)")
-        
-        do {
-            // Read the file contents
-            FactoryCount = try Int(String(contentsOf: fileURL))!
-        } catch let error as NSError {
-            print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
-        }
-        
-        FactoryVariable = 500 * pow(1.15, Double(FactoryCount))
-        
-        
-        
-        
-        print("File Text: \(CookieVariable)")
         NotificationCenter.default.addObserver(self, selector: #selector(updateCookie), name: NSNotification.Name(rawValue: "callForAlert"), object: nil)
         
         if CursorVariable <= 14 {
@@ -212,7 +129,7 @@ class UpgradeViewController: UITableViewController {
         
         do {
             // Write to the file
-            try String(CursorCount).write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+            try String(CursorAmount).write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
         } catch let error as NSError {
             print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
         }
@@ -227,7 +144,7 @@ class UpgradeViewController: UITableViewController {
         
         do {
             // Write to the file
-            try String(GrandmaCount).write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+            try String(GrandmaAmount).write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
         } catch let error as NSError {
             print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
         }
@@ -241,7 +158,7 @@ class UpgradeViewController: UITableViewController {
         
         do {
             // Write to the file
-            try String(MineCount).write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+            try String(MineAmount).write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
         } catch let error as NSError {
             print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
         }
@@ -255,7 +172,7 @@ class UpgradeViewController: UITableViewController {
         
         do {
             // Write to the file
-            try String(FactoryCount).write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+            try String(FactoryAmount).write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
         } catch let error as NSError {
             print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
         }
@@ -269,10 +186,10 @@ class UpgradeViewController: UITableViewController {
     }
     
     @objc func autoClicks() {
-        CookieVariable = Double(CursorCount) / 1000 + CookieVariable // Cursors add .1/S, timer is every 0.01/S  /100
-        CookieVariable = Double(GrandmaCount) / 100 + CookieVariable // Grandmas add 1/S timer is every 0.01/s /100
-        CookieVariable = Double(MineCount) / 100 + CookieVariable // Mines add 3/S timer is every 0.01/s /100
-        CookieVariable = Double(FactoryCount) / 100 + CookieVariable // Mines add 5/S timer is every 0.01/s /100
+        CookieVariable = Double(CursorAmount) / 1000 + CookieVariable // Cursors add .1/S, timer is every 0.01/S  /100
+        CookieVariable = Double(GrandmaAmount) / 100 + CookieVariable // Grandmas add 1/S timer is every 0.01/s /100
+        CookieVariable = Double(MineAmount) / 100 + CookieVariable // Mines add 3/S timer is every 0.01/s /100
+        CookieVariable = Double(FactoryAmount) / 100 + CookieVariable // Factories add 5/S timer is every 0.01/s /100
 
         
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "callForAlert"), object: nil)
@@ -285,35 +202,35 @@ class UpgradeViewController: UITableViewController {
     
     @IBOutlet weak var CursorBuy: UIButton!
     @IBOutlet weak var CursorLabel: UILabel!
-    @IBOutlet weak var CursorAmount: UILabel!
+    @IBOutlet weak var CursorAmountLabel: UILabel!
     
     @IBOutlet weak var GrandmaBuy: UIButton!
     @IBOutlet weak var GrandmaLabel: UILabel!
-    @IBOutlet weak var GrandmaAmount: UILabel!
+    @IBOutlet weak var GrandmaAmountLabel: UILabel!
     
     @IBOutlet weak var MineBuy: UIButton!
     @IBOutlet weak var MineLabel: UILabel!
-    @IBOutlet weak var MineAmount: UILabel!
+    @IBOutlet weak var MineAmountLabel: UILabel!
     
     @IBOutlet weak var FactoryBuy: UIButton!
     @IBOutlet weak var FactoryLabel: UILabel!
-    @IBOutlet weak var FactoryAmount: UILabel!
+    @IBOutlet weak var FactoryAmountLabel: UILabel!
     
     func initCursor() {
         self.CursorLabel.text = String(format: "%.0f", CursorVariable)
-        self.CursorAmount.text = "\(CursorCount)"
+        self.CursorAmountLabel.text = "\(CursorAmount)"
     }
     func initGrandma() {
         self.GrandmaLabel.text = String(format: "%.0f", GrandmaVariable)
-        self.GrandmaAmount.text = "\(GrandmaCount)"
+        self.GrandmaAmountLabel.text = "\(GrandmaAmount)"
     }
     func initMine() {
         self.MineLabel.text = String(format: "%.0f", MineVariable)
-        self.MineAmount.text = "\(MineCount)"
+        self.MineAmountLabel.text = "\(MineAmount)"
     }
     func initFactory() {
         self.FactoryLabel.text = String(format: "%.0f", FactoryVariable)
-        self.FactoryAmount.text = "\(FactoryCount)"
+        self.FactoryAmountLabel.text = "\(FactoryAmount)"
     }
     
     @IBAction func AddCursor(_ sender: UIButton) {
@@ -322,10 +239,10 @@ class UpgradeViewController: UITableViewController {
             
             CookieVariable = CookieVariable - Double(CursorVariable)
             
-            CursorCount = CursorCount + 1
+            CursorAmount = CursorAmount + 1
             CursorVariable = CursorVariable * 1.1
             self.CursorLabel.text = String(format: "%.0f", CursorVariable)
-            self.CursorAmount.text = "\(CursorCount)"
+            self.CursorAmountLabel.text = "\(CursorAmount)"
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: "callForAlert"), object: nil)
         }
         
@@ -338,10 +255,10 @@ class UpgradeViewController: UITableViewController {
             
             CookieVariable = CookieVariable - Double(GrandmaVariable)
             
-            GrandmaCount = GrandmaCount + 1
+            GrandmaAmount = GrandmaAmount + 1
             GrandmaVariable = GrandmaVariable * 1.1
             self.GrandmaLabel.text = String(format: "%.0f", GrandmaVariable)
-            self.GrandmaAmount.text = "\(GrandmaCount)"
+            self.GrandmaAmountLabel.text = "\(GrandmaAmount)"
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: "callForAlert"), object: nil)
         }
         
@@ -355,10 +272,10 @@ class UpgradeViewController: UITableViewController {
             
             CookieVariable = CookieVariable - Double(MineVariable)
             
-            MineCount = MineCount + 1
+            MineAmount = MineAmount + 1
             MineVariable = MineVariable * 1.1
             self.MineLabel.text = String(format: "%.0f", MineVariable)
-            self.MineAmount.text = "\(MineCount)"
+            self.MineAmount.text = "\(MineAmount)"
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: "callForAlert"), object: nil)
         }
         
@@ -372,10 +289,10 @@ class UpgradeViewController: UITableViewController {
             
             CookieVariable = CookieVariable - Double(FactoryVariable)
             
-            FactoryCount = FactoryCount + 1
+            FactoryAmount = FactoryAmount + 1
             FactoryVariable = FactoryVariable * 1.1
             self.FactoryLabel.text = String(format: "%.0f", FactoryVariable)
-            self.FactoryAmount.text = "\(FactoryCount)"
+            self.FactoryAmount.text = "\(FactoryAmount)"
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: "callForAlert"), object: nil)
         }
     
